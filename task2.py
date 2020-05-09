898dd1427fbca35a5a15f1eada2c745523c310b6
Oren Shor <shororen@post.bgu.ac.il>
2020-05-09 10:24:32 +0300

    uploading file "task2b"

--- task2.py
+++ task2.py
@@ -40,15 +40,21 @@ input_test = input_test / 255
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+
 model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+
+
 model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+
 model.add(Flatten())
 model.add(Dense(256, activation='relu'))
 model.add(Dense(128, activation='relu'))
 model.add(Dense(no_classes, activation='softmax'))
 
+shit = [] 
+shit.a
 # model = Sequential()
 # # model.add(ZeroPadding2D(4, input_shape=input_train.shape[1:]))
 # model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
